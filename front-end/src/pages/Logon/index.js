40007c9f0d636fc811aff0c2f948d5ea27212bd7
Jhonny <jhonnyimmbe@gmail.com>
2020-03-26 19:23:59 -0300

    Front-End 1.1

--- front-end/src/pages/Logon/index.js
+++ front-end/src/pages/Logon/index.js
@@ -1,51 +1,23 @@
-import React ,{useState} from 'react';
-import { Link, useHistory } from 'react-router-dom';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
-import api from '../../services/api';
+
 import './style.css';
 
 import logoImg from '../../assets/logo.svg';
 import heroesImg from '../../assets/heroes.png';
 
 export default function Logon() {
-    const [id, setId] = useState('');
-    const history =  useHistory();
-    async function handleLogin(e){
-        e.preventDefault(); 
-
-        try {
-            const response = await api.post('sessions',{ id });
-            console.log(response);
-            
-            localStorage.setItem('ongId',id);
-            localStorage.setItem('ongName',response.data.name);
-
-            console.log(response.data.name);   
-            
-            history.push('/profile');
-        }catch (err) {
-            alert('Falha no login, tente novamente');
-            console.log('faill');
-            /** não esta avinsando quando não conegue fazer login RESOLVER  */
-        }
-    }
-
     return (
         <div className="logon-container">
             <section className="form">
 
              <img src={logoImg} alt="Be The Hero"/>
 
-             <form onSubmit={ handleLogin }>
+             <form>
                 <h1>Faça seu logon</h1>
                 
-                <input 
-                    placeholder="Sua ID"
-                    required
-                    value={id}
-                    onChange={e => setId(e.target.value)}
-                 />
-
+                <input placeholder="Sua ID" />
                 <button className="button" type="submit">Entrar</button>
 
                 <Link className="back-link " to="/register">
--- front-end/src/pages/Profile/index.js
+++ front-end/src/pages/Profile/index.js
@@ -1,88 +1,23 @@
-import React, {useState ,useEffect} from 'react';
-import { Link, useHistory } from 'react-router-dom';
-import { FiPower,FiTrash2 } from 'react-icons/fi';
-
-import api from '../../services/api' 
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPower } from 'react-icons/fi';
 
 import './style.css';
 
 import logoImg from "../../assets/logo.svg"
 
 export default function Profile(){
-
-    const [incidents, setIncidents] = useState([]);
-
-    const history = useHistory();
-
-    const ongId = localStorage.getItem('ongId');
-    const ongName = localStorage.getItem('ongName');
-
-    useEffect(() => {
-        api.get('profile',{
-            headers:{
-                Authorization:ongId,
-            }
-        }).then(response => {
-            setIncidents(response.data)
-        })
-    },[ongId]);
-
-    async function handleDeleteIncidents(id){
-        try {
-           await api.delete(`incidents/${id}`,{
-             headers:{
-                 Authorization:ongId,
-             }  
-           }); 
-
-           setIncidents(incidents.filter(incident => incident.id !== id));
-
-        } catch (error) {
-            alert('Erro ao deletar caso, tente novamente.')
-        }
-    }
-
-    async function handleLogout(){
-        localStorage.clear();
-
-        history.push('/');
-    }
-
     return (
         <div className="profile-container">
             <header>
                 <img src={logoImg} alt="Be the Hero"/>
-            <span> Bem Vinda, {ongName} </span>
+            <span> Bem Vinda, APAD</span>
             
             <Link className="button" to="/incidents/new"> Cadastrar novo casos </Link>
-            <button onClick={handleLogout} type="button">
+            <button type="button">
                 <FiPower size={18} color="#e02041"/>
             </button>
-        </header>
-
-        <h1> Casos cadastrados </h1>
-
-        <ul>
-            {incidents.map(incident => (
-
-                <li key={incident.id} >
-                <strong> Caso: </strong>
-                <p>{incident.title}</p>
-
-                <strong> DESCRIÇÃO: </strong>
-                <p>{incident.description}</p>
-
-                <strong> Valor </strong>
-                <p>{ Intl.NumberFormat('pt-BR',{ style: 'currency',currency: 'BRL'}).format( incident.value)}</p>
-
-                <button onClick={() => handleDeleteIncidents(incident.id)}>
-                    <FiTrash2 size={20} color="#a8a8b3"/>
-                </button>
-            </li>
-
-            ))}        
-            
-        </ul>
+            </header>
         </div> 
     );
 }
